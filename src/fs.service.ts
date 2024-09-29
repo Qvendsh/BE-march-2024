d66d5daad38ccf316f289054514c7b261e658bca
Qvendsh bc2024-2 <qvendsh@gmail.com>
2024-09-29 23:06:33 +0300

    before husky

--- src/fs.service.ts
+++ src/fs.service.ts
@@ -1,25 +1,25 @@
-import fs from'node:fs/promises'
-import path from 'node:path'
+import fs from "node:fs/promises";
+import path from "node:path";
 
+import { IUser } from "./interfaces/user.interface";
 
+const read = async (): Promise<IUser[]> => {
+  try {
+    const pathToFile = path.join(process.cwd(), "db.json");
+    const data = await fs.readFile(pathToFile, "utf-8");
+    return data ? JSON.parse(data) : [];
+  } catch (e) {
+    console.log("read mistake", e.message);
+  }
+};
 
-const read = async () =>{
-   try {
-       const pathToFile = path.join(__dirname, 'db.json')
-       const data = await fs.readFile(pathToFile, 'utf-8')
-       return data ? JSON.parse(data) : []
-   }catch (e){
-       console.log("read mistake", e.message)
-   }
-}
+const write = async (users: IUser[]): Promise<void> => {
+  try {
+    const pathToFile = path.join(process.cwd(), "db.json");
+    await fs.writeFile(pathToFile, JSON.stringify(users));
+  } catch (e) {
+    console.log("write mistake", e.message);
+  }
+};
 
-const write = async (users) =>{
-   try {
-       const pathToFile = path.join(__dirname, 'db.json')
-       await fs.writeFile(pathToFile, JSON.stringify(users))
-   }catch (e){
-       console.log("write mistake", e.message)
-   }
-}
-
-export {read,write}
+export { read, write };
